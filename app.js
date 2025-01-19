b330222c8c50a250f8b4d09c3e4e25b70d4417ac
Pranish <pranishkadel1@gmail.com>
2025-01-19 14:39:35 +0545

    third commit(chat-app)#3

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ import { connectDB } from "./config/database.js";
 import userRoutes from "./routes/userRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import chatRoomRoutes from "./routes/chatRoomRoutes.js";
+import path from "path"
 
 import cors from "cors";
 
 const app = express();
+
 
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.set('views',path.join(__dirname,"views"))
 
 app.use(userRoutes);
 app.use(messageRoutes);
 app.use(chatRoomRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("hi");
